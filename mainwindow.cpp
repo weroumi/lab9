995c18bf995837fe6fbcc5f17079e9b99ed28f99
weroumi <ookk1405@gmail.com>
2022-11-14 19:55:40 +0200

    Created ui, added slot text_edited for combo box

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,10 +6,37 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    ui->gridLayoutWidget->setStyleSheet("background-color: rgb(255, 255)");
+    txtDirectiories[0] = ui->txtDir_1;
+    txtDirectiories[1] = ui->txtDir_2;
+    txtDirectiories[2] = ui->txtDir_3;
+    txtDirectiories[3] = ui->txtDir_4;
+    txtExtencions[0] = ui->txtExtencion_1;
+    txtExtencions[1] = ui->txtExtencion_2;
+    txtExtencions[2] = ui->txtExtencion_3;
+    txtExtencions[3] = ui->txtExtencion_4;
+    for(size_t i = 1; i < MAX_NUM_OF_CLIENTS; ++i){
+        txtExtencions[i]->hide();
+        txtDirectiories[i]->hide();
+    }
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+
+void MainWindow::on_cmbNumOfClients_currentTextChanged(const QString &arg1)
+{
+    for(size_t i = 1; i < MAX_NUM_OF_CLIENTS; ++i){
+        txtExtencions[i]->hide();
+        txtDirectiories[i]->hide();
+    }
+    numOfClients = ui->cmbNumOfClients->currentText().toInt();
+    for(size_t i = 1; i < numOfClients; ++i){
+        txtExtencions[i]->show();
+        txtDirectiories[i]->show();
+    }
 }
 
--- mainwindow.h
+++ mainwindow.h
@@ -1,7 +1,9 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
+#include "qlineedit.h"
 #include <QMainWindow>
+#define MAX_NUM_OF_CLIENTS 4
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -15,7 +17,13 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private slots:
+    void on_cmbNumOfClients_currentTextChanged(const QString &arg1);
+
 private:
     Ui::MainWindow *ui;
+    QLineEdit* txtDirectiories[MAX_NUM_OF_CLIENTS];
+    QLineEdit* txtExtencions[MAX_NUM_OF_CLIENTS];
+    size_t numOfClients;
 };
 #endif // MAINWINDOW_H
